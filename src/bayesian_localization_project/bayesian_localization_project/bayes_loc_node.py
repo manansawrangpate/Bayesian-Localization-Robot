de45a5b1f44a9f46efac3a16c1ecf0cec216ca50
Manan Sawrangpate <manansawrangpate1@gmail.com>
2026-05-18 12:15:52 -0700

    Increase delivery approach to 15s

--- src/bayesian_localization_project/bayesian_localization_project/bayes_loc_node.py
+++ src/bayesian_localization_project/bayesian_localization_project/bayes_loc_node.py
@@ -196,8 +196,8 @@ class BayesLocNode(Node):
         self._stop()
 
     def _deliver_mail(self, office: int):
-        self.get_logger().info(f'*** DELIVERING to office {office} — line-following 10s to centre ***')
-        end = time.time() + 10.0
+        self.get_logger().info(f'*** DELIVERING to office {office} — line-following 15s to centre ***')
+        end = time.time() + 15.0
         while time.time() < end and self._running:
             with self._lock:
                 line = self.cur_line
